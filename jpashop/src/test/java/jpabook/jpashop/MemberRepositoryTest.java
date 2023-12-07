3833b446582aa99fcdbd19ff859eb4223e276703
dlrdyd97 <dlrdyd97@naver.com>
2023-12-07 13:37:54 +0900

    JPA , DB 설정 및 동작확인

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional // 테스트에 @Transactional 작성하면 테스트 끝나고 자동 롤백
+    @Rollback(value = false) // 롤백을 원하지 않으면 작성
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("강태혁");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember = " + findMember);
+        System.out.println("member = " + member);
+
+    }
+
+}
